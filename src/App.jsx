a05881e58483c68af74892678a39227aa4e711e1
TakshaSharma <taksha.sharma8@gmail.com>
2024-09-19 14:44:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './components/Todo'
+const App = () => {
+  return (
+    <div className='bg-blue-200 grid py-4 text-white h-screen'>
+     <Todo/>
+    </div>
+  )
+}
+
+export default App
